3f927101b56b43f8e81a68efd28799ab841a9a71
A758023 <A758023@DESKTOP-9CMA8SD>
2022-09-30 18:35:54 +0530

    commit

--- src/main/java/net/atos/air/user/controller/UserRegistrationApiController.java
+++ src/main/java/net/atos/air/user/controller/UserRegistrationApiController.java
@@ -1,6 +1,5 @@
 package net.atos.air.user.controller;
 
-
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -16,46 +15,43 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import net.atos.air.user.dto.UserDetailsdto;
-import net.atos.air.user.entity.UserDetails;
 import net.atos.air.user.service.UserService;
 
 @RestController
 public class UserRegistrationApiController {
-	
-	@Autowired  
-	UserService userService; 
-	
+
+	@Autowired
+	UserService userService;
+
 	@Autowired
 	private ModelMapper modelMapper;
-	
-	 @Autowired
-	 private Environment env;
-	 
-	 @Autowired
-	 ObjectMapper objectMapper;
-	
-	@PostMapping("/userRegistration") 
-	private ResponseEntity<Map<String, String>>  userRegistration(@Valid @RequestBody UserDetailsdto users)   
-	{  
-		if(!userService.findByUserName(users.getUserName())) {
-			userService.userRegistration(users);  
-			return new ResponseEntity<Map<String, String>>(Collections.singletonMap("Success", env.getProperty("successMessage")), HttpStatus.CREATED);
-		}
-		else 
-			return new ResponseEntity<Map<String, String>>(Collections.singletonMap("Error", env.getProperty("userexist")), HttpStatus.ALREADY_REPORTED);
-	}  
 
-	@GetMapping("/getUserDetails") 
-	private List<UserDetailsdto> getAllUsers()   
-	{  
+	@Autowired
+	private Environment env;
+
+	@Autowired
+	ObjectMapper objectMapper;
+
+	@PostMapping("/userRegistration")
+	private ResponseEntity<Map<String, String>> userRegistration(@Valid @RequestBody UserDetailsdto users) {
+		if (!userService.findByUserName(users.getUserName())) {
+			userService.userRegistration(users);
+			return new ResponseEntity<Map<String, String>>(
+					Collections.singletonMap("Success", env.getProperty("successMessage")), HttpStatus.CREATED);
+		} else
+			return new ResponseEntity<Map<String, String>>(
+					Collections.singletonMap("Error", env.getProperty("userexist")), HttpStatus.ALREADY_REPORTED);
+	}
+
+	@GetMapping("/getUserDetails")
+	private List<UserDetailsdto> getAllUsers() {
 		return userService.getAllUser().stream().map(post -> modelMapper.map(post, UserDetailsdto.class))
-			.collect(Collectors.toList());
-	}  
-	
+				.collect(Collectors.toList());
+	}
+
 }
